d0db50da8740cd1178738cdd636de5cadd8c509f
Yuriy Peskov <yuriy.peskov@gmail.com>
2025-05-31 20:40:13 +0300

    added alembic and db

---
+++ app/config/settings.py
@@ -0,0 +1,44 @@
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str = "prod"
+    FRONTEND_URL: str = "https://oktal.ai"
+
+    DB_USER: str = "username"
+    DB_PASSWORD: str = "userpassword"
+    DB_HOST: str = "db-budgeter"
+    DB_NAME: str = "dbname"
+    DB_PORT: int = 5432
+
+    CELERY_BROKER_URL: str = "redis://redis-budgeter:6379"
+    CELERY_RESULT_BACKEND: str = "redis://redis-budgeter:6379"
+
+    TEST_LOG_FILE: str = "test.log"
+
+    DAILY_DB_BACKUP_HOUR: str = "14"
+    DAILY_DB_BACKUP_MINUTE: str = "00"
+    DB_BACKUP_DIR: str = "backup"
+
+    ADMINS_NOTIFICATION_EMAILS: list[str] = []
+
+    MAIL_USERNAME: str = "example@example.com"
+    MAIL_PASSWORD: str = "*************"
+    MAIL_FROM: str = "example@example.com"
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_FROM_NAME: str = "OrgFin.run Team"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+
+    # JWT expiration time in minutes
+    LOGIN_SESSION_EXPIRATION_MINUTES: int = 30
+
+    GOOGLE_CLIENT_ID: str = "123"
+    SECRET_KEY: str = "111111111"
+
+    model_config = SettingsConfigDict(env_file=(".env", ".env.local", ".env.prod"))
+
+
+settings = Settings()
---
+++ app/core/database/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.config.settings import settings as s
+
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{s.DB_USER}:{s.DB_PASSWORD}@{s.DB_HOST}:{s.DB_PORT}/{s.DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():  # pragma: no cover
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
